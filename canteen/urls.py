986251526b5804bb39bac26245600126a08384ae
Aleksey Petukhov <55652160+ikazman@users.noreply.github.com>
2026-09-15 13:36:17 +0300

    Make menu navigation open archive

--- canteen/urls.py
+++ canteen/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("menu/", views.menu_today, name="menu-today"),
+    path("menu/", views.menu_archive, name="menu-archive"),
     path("menu/<str:menu_date>/", views.menu_detail, name="menu-detail"),
     path("editor/menu/", views.editor_menu, name="editor-menu"),
 ]
