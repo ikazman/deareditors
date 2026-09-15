281572192ae646faa7ec7cf478d4e76a2907cdb8
Aleksey Petukhov <55652160+ikazman@users.noreply.github.com>
2026-09-15 12:54:52 +0300

    Fix canteen date route

--- canteen/urls.py
+++ canteen/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("menu/", views.menu_today, name="menu-today"),
-    path("menu/<date:menu_date>/", views.menu_detail, name="menu-detail"),
+    path("menu/<str:menu_date>/", views.menu_detail, name="menu-detail"),
     path("editor/menu/", views.editor_menu, name="editor-menu"),
 ]
